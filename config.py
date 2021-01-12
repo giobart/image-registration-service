96155039091e1b7c5bc7fc5b44c622d427788244
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2021-01-12 21:09:22 +0100

    added mongodb protocol as env

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ from credentials import username, password
 import os
 
 # -*- coding: utf-8 -*-
+DB_PROTOCOL = os.getenv('MONGO_DB_PROTOCOL', "mongodb+srv")
 DB_URL = os.getenv('MONGO_DB_URL', 'cluster0.pwgbi.mongodb.net/whosthatpokemon')
-DB_ADDRESS = "mongodb+srv://" + username + ":" + password + "@" + DB_URL + "?retryWrites=true&w=majority"
+DB_ADDRESS = DB_PROTOCOL+"://" + username + ":" + password + "@" + DB_URL + "?retryWrites=true&w=majority"
 IMG_MODEL_FOLDER = os.path.join(".", "data", "net")
 IMG_MODEL_PATH = os.path.join(IMG_MODEL_FOLDER, "model.ckpt")
 IMG_MODEL_URL = "https://drive.google.com/uc?id=1RXTbnrlwEhb5G0F6TfXZCVG3GC19BT-l"
